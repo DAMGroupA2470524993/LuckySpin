9b6f7a8a5eae9d473201e4008848c6c13ef45995
DLarangeira <diogo.f.larangeira@gmail.com>
2025-01-29 19:02:49 +0000

    Segunda fase da roleta. -Adiçao de gifs roleta -Adição dos botões de aposta -Adição do botão que fará a roleta girar

--- app/src/main/java/pt/ipt/dam/luckyspin/RoletaActivity.kt
+++ app/src/main/java/pt/ipt/dam/luckyspin/RoletaActivity.kt
@@ -1,6 +1,7 @@
 package pt.ipt.dam.luckyspin
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.graphics.ImageDecoder
 import android.graphics.drawable.AnimatedImageDrawable
 import android.graphics.drawable.Drawable
@@ -12,6 +13,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -25,17 +27,19 @@ class RoletaActivity : AppCompatActivity() {
     lateinit var f1: tituloApp
 
     lateinit var image: ImageView
-    lateinit var btImage: ImageButton
+    lateinit var roulette: ImageView
 
+    private var betBlack: Boolean = false
+    private var betRed: Boolean = false
+    private var betGreen: Boolean = false
 
-    private lateinit var sensorManager: SensorManager
+    /*private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
     private var lastShakeTime: Long = 0
-    private var shakeThreshold = 12.0f
+    private var shakeThreshold = 12.0f*/
 
 
-
-    @SuppressLint("NewApi")
+    @SuppressLint("NewApi", "WrongThread")
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,9 +51,6 @@ class RoletaActivity : AppCompatActivity() {
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
 
-        //iniciar utilizador
-        val user = User()
-
 
         //Mostrar o Gif com animação
         val source :ImageDecoder.Source = ImageDecoder.createSource(
@@ -60,27 +61,169 @@ class RoletaActivity : AppCompatActivity() {
         image.setImageDrawable(drawable)
         (drawable as? AnimatedImageDrawable)?.start()
 
-        //botão imagem da roleta
-        btImage = findViewById(R.id.rouletteWheel)
-        btImage.setOnClickListener {
+        //Mostrar o Gif com animação
+        val sourceR :ImageDecoder.Source = ImageDecoder.createSource(
+            resources, R.drawable.roleta_gif
+        )
+        val drawableR : Drawable = ImageDecoder.decodeDrawable(sourceR)
+        roulette = findViewById(R.id.rouletteWheel)
+        roulette.setImageDrawable(drawableR)
+        (drawableR as? AnimatedImageDrawable)?.start()
 
-            //verifica se os creditos são maiores ou iguais aos créditos
-            if (){
-                //caso seja maior ou igual
-                //1- tiro o dinheiro
-                //2- gira a rouleta
-                spinRoulette()
+        var btBlack : Button = findViewById(R.id.btBlack)
+        var btRed : Button = findViewById(R.id.btRed)
+        var btGreen : Button = findViewById(R.id.btGreen)
+        var btSpin : Button = findViewById(R.id.btSpin)
 
+        val betButtons = listOf(btBlack, btRed, btGreen)
+
+        var selectBt: Button? = null
+
+        fun betButtonSelected(bt: Button) {
+            if (bt == selectBt) {
+                betButtons.forEach { it.isEnabled = true }
+                selectBt = null
             } else {
-                //caso seja menor
-                //Falta de créditos
-                Toast.makeText(this,"Falta créditos!!",Toast.LENGTH_LONG).show()
+                betButtons.forEach { it.isEnabled = it == bt }
+                selectBt = bt
             }
-
-
+        }
+        fun resetBt() {
+            betButtons.forEach { it.isEnabled = true }
+            selectBt = null
+            betBlack = false
+            betRed = false
+            betGreen = false
+            btBlack.setBackgroundColor(Color.BLACK)
+            btRed.setBackgroundColor(Color.RED)
+            btGreen.setBackgroundColor(Color.GREEN)
         }
 
-        /*// Sensor setup
+
+        btBlack.setOnClickListener{
+            betButtonSelected(btBlack)
+            if (betBlack == false) {
+                betBlack = true
+                btBlack.setBackgroundColor(Color.BLUE)
+            } else {
+                betBlack = false
+                btBlack.setBackgroundColor(Color.BLACK)
+            }
+        }
+        btRed.setOnClickListener{
+            betButtonSelected(btRed)
+            if (betRed == false) {
+                betRed = true
+                btRed.setBackgroundColor(Color.BLUE)
+            } else {
+                betRed = false
+                btRed.setBackgroundColor(Color.RED)
+            }
+        }
+        btGreen.setOnClickListener{
+            betButtonSelected(btGreen)
+            if (betGreen == false) {
+                betGreen = true
+                btGreen.setBackgroundColor(Color.BLUE)
+            } else {
+                betGreen = false
+                btGreen.setBackgroundColor(Color.GREEN)
+            }
+        }
+
+
+
+
+        btSpin.setOnClickListener{
+            //gerar um número entre 1 e 10
+            //1, 3, 6, 8 - > preto
+            //2, 4, 7, 9 - > vermelho
+            //5, 10      - > verde
+
+            var randNum = Random.nextInt(10) + 1 // valores entre 1 e 7
+
+            if (betBlack == false && betRed == false && betGreen == false){
+                Toast.makeText(this, "Please make a bet!", Toast.LENGTH_SHORT).show()
+            } else {
+                spinRoulette(randNum)
+                checkColor(randNum)
+                checkWin(checkColor(randNum))
+            }
+            resetBt()
+        }
+
+    }
+
+    private fun spinRoulette(n : Int) {
+        var imagetoRoulette = when (n) {
+            1 -> R.drawable.roleta1
+            2 -> R.drawable.roleta2
+            3 -> R.drawable.roleta3
+            4 -> R.drawable.roleta4
+            5 -> R.drawable.roleta5
+            6 -> R.drawable.roleta6
+            7 -> R.drawable.roleta7
+            8 -> R.drawable.roleta8
+            9 -> R.drawable.roleta9
+            10 -> R.drawable.roleta10
+            else -> R.drawable.roleta
+        }
+        roulette.setImageResource(imagetoRoulette)
+    }
+
+    private fun checkColor(n : Int) : String {
+        if (n == 1 || n == 3 || n == 6 || n == 8) {
+            return "preto"
+        } else{
+            if (n == 2 || n == 4 || n == 7 || n == 9) {
+                return "vermelho"
+            } else {
+                return "verde"
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun checkWin(str : String) {
+        if ((str == "preto" && betBlack == true) || (str == "vermelho" && betRed == true)) {
+            val sourceRes :ImageDecoder.Source = ImageDecoder.createSource(
+                resources, R.drawable.win
+            )
+            val drawableRes : Drawable = ImageDecoder.decodeDrawable(sourceRes)
+            image.setImageDrawable(drawableRes)
+            (drawableRes as? AnimatedImageDrawable)?.start()
+        } else {
+                if (str == "verde" && betGreen == true){
+                    val sourceRes :ImageDecoder.Source = ImageDecoder.createSource(
+                        resources, R.drawable.jackpot
+                    )
+                    val drawableRes : Drawable = ImageDecoder.decodeDrawable(sourceRes)
+                    image.setImageDrawable(drawableRes)
+                    (drawableRes as? AnimatedImageDrawable)?.start()
+                } else {
+                        val sourceRes :ImageDecoder.Source = ImageDecoder.createSource(
+                            resources, R.drawable.lose
+                        )
+                        val drawableRes : Drawable = ImageDecoder.decodeDrawable(sourceRes)
+                        image.setImageDrawable(drawableRes)
+                        (drawableRes as? AnimatedImageDrawable)?.start()
+                }
+            }
+        }
+
+
+
+
+
+ ///////////////////////////////////////////////////////////////
+   /* override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(this)
+    }*/
+}
+
+//dentro do onCreate
+/*// Sensor setup
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         if (accelerometer == null) {
@@ -88,9 +231,8 @@ class RoletaActivity : AppCompatActivity() {
         } else {
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
         }*/
-    }
-
-    /*override fun onSensorChanged(event: SensorEvent?) {
+////////////////////////////////////////////////
+/*override fun onSensorChanged(event: SensorEvent?) {
         if (event == null) return
 
         val x = event.values[0]
@@ -106,82 +248,3 @@ class RoletaActivity : AppCompatActivity() {
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}*/
-
-
-    @SuppressLint("NewApi")
-    @RequiresApi(Build.VERSION_CODES.P)
-    private fun spinRoulette() {
-       /* val randomAngle = Random.nextInt(360, 7200) // Large number for multiple spins
-        val animation = RotateAnimation(
-            0f, randomAngle.toFloat(),
-            Animation.RELATIVE_TO_SELF, 0.5f,
-            Animation.RELATIVE_TO_SELF, 0.5f
-        ).apply {
-            duration = 3000 // Spin duration
-            fillAfter = true
-        }
-
-        // Start animation
-        rouletteWheel.startAnimation(animation)
-
-        // Show result after spin
-        rouletteWheel.postDelayed({
-            val result = (randomAngle % 37) // Roulette has 37 slots (0-36)
-            resultText.text = "Result: $result"
-        }, 3000) */
-
-
-        //COMO EU ACHO QUE FAZIAMOS QUE ERA MAIS SIMPLES
-        //GERAR UM NÚMERO ATÉ 7 APENAS
-        //1,3,5 -> GANHAVA
-        //2,4,6 -> PERDIA
-        //7 -> SUPER JACKPOT
-
-        //1
-        var randNum = Random.nextInt(7) + 1 // valores entre 1 e 7
-
-        //Verifica o número Random e adequa o a imagem a aparecer
-        if (randNum == 1 || randNum == 3 || randNum == 5 ){
-            val source :ImageDecoder.Source = ImageDecoder.createSource(
-                resources, R.drawable.win
-            )
-            val drawable : Drawable = ImageDecoder.decodeDrawable(source)
-            image.setImageDrawable(drawable)
-            (drawable as? AnimatedImageDrawable)?.start()
-        } else {
-            if (randNum == 2 || randNum == 4 || randNum == 6) {
-                val source :ImageDecoder.Source = ImageDecoder.createSource(
-                    resources, R.drawable.lose
-                )
-                val drawable : Drawable = ImageDecoder.decodeDrawable(source)
-                image.setImageDrawable(drawable)
-                (drawable as? AnimatedImageDrawable)?.start()
-            } else {
-                if (randNum == 7) {
-                    val source: ImageDecoder.Source = ImageDecoder.createSource(
-                        resources, R.drawable.jackpot
-                    )
-                    val drawable: Drawable = ImageDecoder.decodeDrawable(source)
-                    image.setImageDrawable(drawable)
-                    (drawable as? AnimatedImageDrawable)?.start()
-                }
-            }
-        }
-
-
-
-    }
-
-   /* override fun onDestroy() {
-        super.onDestroy()
-        sensorManager.unregisterListener(this)
-    }*/
-}
-
-data class User(
-    val id: Int,
-    val email: String,
-    val creditos: Int
-){
-    fun user(id: Int)
-}
